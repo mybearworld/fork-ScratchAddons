b2f59786928c949ab55382e518e703958dba2142
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-11-09 16:51:02 +0100

    Revert "Close onion-skinning/snapping modals when clicking outside of them (#6801)"
    This reverts commit e78a548582b6b005a54a3394fa68a017670418b6.

--- addons/onion-skinning/userscript.js
+++ addons/onion-skinning/userscript.js
@@ -584,10 +584,6 @@ export default async function ({ addon, console, msg }) {
   settingButton.title = msg("settings");
   settingButton.appendChild(createButtonImage("settings"));
 
-  document.body.addEventListener("click", (e) => {
-    if (areSettingsOpen() && !e.target.matches(".sa-onion-settings *")) setSettingsOpen(false);
-  });
-
   //
   // Settings page
   //
